85df2ffe793b7632c59f32baf54b0135fb238030
Lambda125 <927405+lambda125@users.noreply.github.com>
2021-06-06 01:55:21 +1000

    WIP - More progress on signing and placing new order

--- swagger-gen/csharp/src/IO.Swagger.Test/Api/OrderApiTests.cs
+++ swagger-gen/csharp/src/IO.Swagger.Test/Api/OrderApiTests.cs
@@ -9,16 +9,8 @@
  */
 
 using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Reflection;
-using RestSharp;
 using NUnit.Framework;
 using System.Threading.Tasks;
-
-using IO.Swagger.Client;
 using IO.Swagger.Api;
 
 using BybitOrderResponse = IO.Swagger.Model.OrderResBase;
@@ -107,7 +99,7 @@ namespace IO.Swagger.Test
             //var response = instance.OrderGetOrders(symbol, limit, orderStatus, direction, cursor);
             //Assert.IsInstanceOf<Object> (response, "response is Object");
         }
-        
+
         /// <summary>
         /// Test OrderNew
         /// </summary>
@@ -122,28 +114,16 @@ namespace IO.Swagger.Test
             string orderType = "Limit";
             decimal? qty = 1;
             string timeInForce = "GoodTillCancel";
-            double? price = 20;
+            double? price = 30000;
             double? takeProfit = null;
             double? stopLoss = null;
             bool? reduceOnly = null;
             bool? closeOnTrigger = null;
             string orderLinkId = "abcd";
-            string timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds().ToString();
-            
-            Console.Out.WriteLine("Timestamp --> "+timestamp);
-            Dictionary<String, String> parameters = new Dictionary<string, string>();
-            parameters.Add("side", side);
-            parameters.Add("symbol", symbol);
-            parameters.Add("orderType", orderType);
-            parameters.Add("qty",qty.ToString());
-            parameters.Add("timeInForce", timeInForce);
-            parameters.Add("price", price.ToString());
-            parameters.Add("orderLinkId", orderLinkId);
-            parameters.Add("timestamp", timestamp); 
-            parameters.Add("api_key", "w0ddzkzS3kMExz4Xpc");
-            instance.Configuration.AddApiKeyPrefix("api_key", "w0ddzkzS3kMExz4Xpc");
-            instance.Configuration.AddApiKeyPrefix("sign", Common.getSignature("ppsAOlX859PcREd3nOjJ5LAJwg39UGB1S2t2", parameters));
-            instance.Configuration.AddApiKeyPrefix("timestamp",timestamp);
+
+            instance.Configuration.AddApiKey("api_key", "w0ddzkzS3kMExz4Xpc");
+            instance.Configuration.AddApiKey("api_secret", "ppsAOlX859PcREd3nOjJ5LAJwg39UGB1S2t2");
+
             var response = await instance.OrderNewAsync(side, 
                 symbol, orderType, qty, 
                 timeInForce, price, takeProfit, 
@@ -153,8 +133,8 @@ namespace IO.Swagger.Test
             var jobj = (Newtonsoft.Json.Linq.JObject) response;
 
             var orderResponse = jobj.ToObject<BybitOrderResponse>(); // TODO do both COINM/USDT return the same type?
-            Console.Out.WriteLine("Log statement to debug"+orderResponse);
-            Assert.IsInstanceOf<Object> (orderResponse, "response is Object");
+            Console.Out.WriteLine("Log statement to debug " + orderResponse);
+            Assert.AreEqual(0, orderResponse.RetCode);
         }
         
         /// <summary>
---
+++ swagger-gen/csharp/src/IO.Swagger/Client/ApiClient.custom.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using RestSharp;
+
+namespace IO.Swagger.Client
+{
+    partial class ApiClient
+    {
+        partial void InterceptRequest(IRestRequest request)
+        {
+            if (Configuration.ApiKey.TryGetValue("api_secret", out var apiSecret))
+            {
+                request.AddOrUpdateParameter("timestamp", DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
+
+                var sign = CreateSignature(apiSecret, request.Parameters);
+                request.AddOrUpdateParameter("sign", sign);
+
+                Configuration.ApiKey.Remove("api_secret");
+            }
+        }
+
+        private static string GetParameterString(IEnumerable<Parameter> parameters)
+            => String.Join("&",
+                parameters
+                    .Where(p => p.Value != null && 
+                               (p.Type == ParameterType.QueryString || p.Type == ParameterType.GetOrPost))
+                    .OrderBy(p => p.Name)
+                    .Select(p => $"{p.Name}={p.Value}")
+               );
+        
+        private static string CreateSignature(string secret, IEnumerable<Parameter> parameters)
+        {
+            var message = GetParameterString(parameters);
+            var signatureBytes = Hmacsha256(Encoding.UTF8.GetBytes(secret), Encoding.UTF8.GetBytes(message));
+            return ByteArrayToString(signatureBytes);
+        }
+
+        private static byte[] Hmacsha256(byte[] keyByte, byte[] messageBytes)
+        {
+            using (var hash = new HMACSHA256(keyByte))
+            {
+                return hash.ComputeHash(messageBytes);
+            }
+        }
+
+        private static string ByteArrayToString(byte[] ba)
+        {
+            var hex = new StringBuilder(ba.Length * 2);
+
+            foreach (var b in ba)
+            {
+                hex.AppendFormat("{0:x2}", b);
+            }
+            return hex.ToString();
+        }
+    }
+}
